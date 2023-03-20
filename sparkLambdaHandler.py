4c866ad9fcb289e9c319772473519b33996c3e23
John Cherian <42749436+JohnChe88@users.noreply.github.com>
2023-03-20 10:44:58 -0400

    Update sparkLambdaHandler.py
    Updated the Lambda handler script with logger and modularized it

--- sparkLambdaHandler.py
+++ sparkLambdaHandler.py
@@ -2,27 +2,65 @@ import boto3
 import sys
 import os
 import subprocess
+import logging
+
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def s3_script_download(s3_bucket_script: str,input_script: str)-> None:
+    """
+    """
+    s3_client = boto3.resource("s3")
+
+    try:
+        logger.info(f'Now downloading script {input_script} in {s3_bucket_script} to /tmp')
+        s3_client.Bucket(s3_bucket_script).download_file(input_script, "/tmp/spark_script.py")
+      
+    except Exception as e :
+        logger.error(f'Error downloading the script {input_script} in {s3_bucket_script}: {e}')
+    else:
+        logger.info(f'Script {input_script} successfully downloaded to /tmp')
+
+
+
+def spark_submit(s3_bucket_script: str,input_script: str)-> None:
+    """
+    Submits a local Spark script using spark-submit.
+    """
+     # Set the environment variables for the Spark application
+    os.environ["PYSPARK_SUBMIT_ARGS"] = "--master local pyspark-shell"
+
+    # Run the spark-submit command on the local copy of teh script
+    try:
+        logger.info(f'Spark-Submitting the Spark script {input_script} from {s3_bucket_script}')
+        subprocess.run(["spark-submit", "/tmp/spark_script.py"])
+    except Exception as e :
+        logger.error(f'Error Spark-Submit with exception: {e}')
+    else:
+        logger.info(f'Script {input_script} successfully submitted')
 
 def lambda_handler(event, context):
- 
- """
- Lambda_handler is called when the AWS Lambda
- is triggered. The function is downloading file 
- from Amazon S3 location and spark submitting 
- the script in AWS Lambda
- """
- 
- print("start AWS Lambda Handler ......")
- s3_bucket_script = os.environ['SCRIPT_BUCKET']
- input_script = os.environ['SPARK_SCRIPT']
- 
- # Creating an S3 boto3 client
- s3_client = boto3.client("s3")
- s3_client.download_file(s3_bucket_script, input_script, "/tmp/spark_script.py")
- 
- # Set the environment variables for the Spark application
- os.environ["PYSPARK_SUBMIT_ARGS"] = "--master local pyspark-shell"
 
- 
- # Run the spark-submit command on the local copy of teh script
- subprocess.run(["spark-submit", "/tmp/spark_script.py"])
+    """
+    Lambda_handler is called when the AWS Lambda
+    is triggered. The function is downloading file 
+    from Amazon S3 location and spark submitting 
+    the script in AWS Lambda
+    """
+
+    logger.info("******************Start AWS Lambda Handler************")
+    s3_bucket_script = os.environ['SCRIPT_BUCKET']
+    input_script = os.environ['SPARK_SCRIPT']
+
+
+    s3_script_download(s3_bucket_script,input_script)
+    
+    # Set the environment variables for the Spark application
+    spark_submit(s3_bucket_script,input_script)
+   
