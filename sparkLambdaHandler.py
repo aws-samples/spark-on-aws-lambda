d852a031289800c30a6f540a9ddf6792d8cac906
Emerson Antony <emantony@amazon.com>
2023-03-24 10:36:25 +1100

    Test override env variables

--- sparkLambdaHandler.py
+++ sparkLambdaHandler.py
@@ -60,10 +60,9 @@ def lambda_handler(event, context):
     logger.info("******************Start AWS Lambda Handler************")
     s3_bucket_script = os.environ['SCRIPT_BUCKET']
     input_script = os.environ['SPARK_SCRIPT']
-
-
-
-
+    os.environ['INPUT_PATH'] = event.get('INPUT_PATH','')
+    os.environ['OUTPUT_PATH'] = event.get('OUTPUT_PATH', '')
+    logger.info(os.environ)
     s3_script_download(s3_bucket_script,input_script)
     
     # Set the environment variables for the Spark application
