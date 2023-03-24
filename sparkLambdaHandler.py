31b22630dcc1c4fae494f33204a2af28448a926f
Emerson Antony <emantony@amazon.com>
2023-03-24 11:14:08 +1100

    lambda handler changes

--- sparkLambdaHandler.py
+++ sparkLambdaHandler.py
@@ -41,8 +41,7 @@ def spark_submit(s3_bucket_script: str,input_script: str, event: dict)-> None:
     # Run the spark-submit command on the local copy of teh script
     try:
         logger.info(f'Spark-Submitting the Spark script {input_script} from {s3_bucket_script}')
-        subprocess.run(["spark-submit", "/tmp/spark_script.py" ],
-                       env=dict(os.environ,INPUT_PATH=input_path,OUTPUT_PATH=output_path))
+        subprocess.run(["spark-submit", "/tmp/spark_script.py" ])
     except Exception as e :
         logger.error(f'Error Spark-Submit with exception: {e}')
     else:
@@ -62,6 +61,7 @@ def lambda_handler(event, context):
     input_script = os.environ['SPARK_SCRIPT']
     os.environ['INPUT_PATH'] = event.get('INPUT_PATH','')
     os.environ['OUTPUT_PATH'] = event.get('OUTPUT_PATH', '')
+    logger.info('Still in lambda......')
     logger.info(os.environ)
     s3_script_download(s3_bucket_script,input_script)
     
